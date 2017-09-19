617fae18f3fb0186fd2b07bd8816985c8d551c59
IzumiSy <beetle-noise@gmx.com>
2017-09-19 12:36:14 +0900

    Fix typo

--- db/migrate/20170919031609_create_shelves.rb
+++ db/migrate/20170919031609_create_shelves.rb
@@ -2,7 +2,7 @@ ROM::SQL.migration do
   change do
     create_table :shelves do
       primary_key :id
-      column :name, DataTime, null: false
+      column :name, String, null: false
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
