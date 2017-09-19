4ff2d9211b4418980ad7031d61ff053d99407a86
IzumiSy <beetle-noise@gmx.com>
2017-09-19 12:41:11 +0900

    Add shelf_respository

---
+++ repositories/shelf_repository.rb
@@ -0,0 +1,12 @@
+module Repository
+  class Shelf < ROM::Repository[:shelves]
+    include Import["db_adapter"]
+
+    relations :books
+    commands :create
+
+    def initialize(_args)
+      super(_args[:db_adapter])
+    end
+  end
+end
