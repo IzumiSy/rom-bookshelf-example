5cabb536b30be958948f6fde55fcdf5d983175b7
IzumiSy <beetle-noise@gmx.com>
2017-09-22 14:55:08 +0000

    Implement by_id_with_books

--- repositories/author_repository.rb
+++ repositories/author_repository.rb
@@ -15,6 +15,14 @@ module Repository
       authors.by_pk(id).one
     end
 
+    def by_id_with_books(id)
+      authors
+        .by_pk(id)
+        .aggregates(:books)
+        .map_to(Domain::Author)
+        .one
+    end
+
     def query(conditions)
       authors
         .with_books
