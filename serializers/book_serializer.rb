86c8c01425b3f0f5e7d20875e21838d5c8442eeb
IzumiSy <beetle-noise@gmx.com>
2017-09-22 17:58:37 +0900

    Remove author_name

--- serializers/book_serializer.rb
+++ serializers/book_serializer.rb
@@ -3,9 +3,5 @@ require_relative "base_serializer"
 module Domain
   class BookSerializer < Domain::BaseSerializer
     attributes :id, :title, :price
-
-    attribute :author_name do
-      object.author[:name]
-    end
   end
 end
