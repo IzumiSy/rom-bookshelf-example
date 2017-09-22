c5d97e9940333be6a208fd60e0e5fb8d5074cfdc
IzumiSy <beetle-noise@gmx.com>
2017-09-22 18:06:15 +0000

    Add some relation methods

--- relations/books.rb
+++ relations/books.rb
@@ -5,4 +5,16 @@ class Books < ROM::Relation[:sql]
       belongs_to :shelf
     end
   end
+
+  def ids
+    select(:id)
+  end
+
+  def index
+    select(:id, :title, :price)
+  end
+
+  def index_with_timestamps
+    index.select_append(:created_at, :updated_at)
+  end
 end
