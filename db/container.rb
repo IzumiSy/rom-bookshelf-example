63f879c56e25e273826d9579732e3592a6d6abed
IzumiSy <beetle-noise@gmx.com>
2017-09-22 19:57:07 +0900

    Move relations directory

--- db/container.rb
+++ db/container.rb
@@ -3,8 +3,8 @@ class InfrastructureContainer
 
   register "db_adapter" do
     config = ROM::Configuration.new(:sql, "sqlite://app.db")
-    config.auto_registration("./relations")
-    config.auto_registration("./commands")
+    config.auto_registration("../relations")
+    config.auto_registration("../commands")
     container = ROM.container(config)
     container
   end
