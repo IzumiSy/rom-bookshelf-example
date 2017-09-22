1440f1002abf270f478c6f4b64b944243e05d71b
IzumiSy <beetle-noise@gmx.com>
2017-09-22 15:19:34 +0000

    include_links: false

--- serializers/author_serializer.rb
+++ serializers/author_serializer.rb
@@ -2,7 +2,7 @@ require_relative "base_serializer"
 
 module Domain
   class AuthorSerializer < Domain::BaseSerializer
-    has_many :books
+    has_many :books, include_links: false
 
     attributes :id, :name
   end
